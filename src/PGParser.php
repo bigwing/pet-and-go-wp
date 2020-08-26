c9ff2f06920294a3d15be832b26bb2c1d959efb3
Morgan Estes <mestes@bigwing.com>
2020-08-26 12:11:58 -0500

    Simplify data storage and retrieval for pets
    Cut down on the number and complexity of array conversions for pet data.
    Use a simple array for data storage, and do the key
    conversion as part of a getter.

--- src/PGParser.php
+++ src/PGParser.php
@@ -19,33 +19,30 @@ class PGParser {
 	use Utilities;
 
 	/**
-	 * Converts a pet class to aN array.
+	 * Converts a pet class to an array.
 	 *
-	 * @param \BigWing\PetAndGo\Pet $pet    The Pet to convert.
-	 * @param string                $output Optional. Any of ARRAY_A | ARRAY_N | OBJECT | OBJECT_K constants.
-	 *                                      ARRAY_N returns an array of arrays indexed from 0 in the format of
-	 *                                      (('field' => field, 'value' => value), ...).
-	 *                                      ARRAY_A returns an associative array (key => value, ...),
-	 *                                      with the key set to a snake_case version of the XML field name.
+	 * @param \BigWing\PetAndGo\Pet $pet      The Pet to convert.
+	 * @param string                $key_type Optional. 'snake' converts keys to WP-standard snake_case.
+	 *                                        'camel' keeps the CamelCased field names returned by the API.
 	 * @return array The pet data.
 	 */
-	public function pet_to_array( Pet $pet, string $output = ARRAY_A ): array {
-		$pet_array    = array();
-		$output_types = array( ARRAY_A, ARRAY_N );
+	public function pet_to_array( Pet $pet, $key_type = 'camel' ): array {
+		$output_types = array( 'snake', 'camel' );
+		$pet_array    = $pet->get_pet_details_array();
 
-		if ( empty( $output ) || ! in_array( $output, $output_types, true ) ) {
-			$output = ARRAY_A;
+		if ( empty( $key_type ) || ! in_array( strtolower( $key_type ), $output_types, true ) ) {
+			$key_type = 'camel';
 		}
 
-		foreach ( $pet->get_fields() as $field ) {
-			if ( ARRAY_A === $output ) {
-				$pet_array[ $this->decamelize( $field ) ] = $pet->get_field( $field );
-			} else {
-				$pet_array[] = array(
-					'field' => $field,
-					'value' => $pet->get_field( $field ),
-				);
+		if ( 'snake' === $key_type ) {
+			$s = array();
+
+			foreach ( $pet_array as $key => $value ) {
+				$sp[ $this->decamelize( $key ) ] = $value;
+				unset( $pet_array[ $key ] );
 			}
+
+			return $pet_array = $s;
 		}
 
 		return $pet_array;
@@ -73,17 +70,11 @@ class PGParser {
 	/**
 	 * Convert the XML response from the API to an array of pet data.
 	 *
-	 * @param SimpleXMLElement $xml    The XML containing the pet fields.
-	 * @param string           $xpath  The xpath to search for the pets collection.
-	 * @param string           $output Optional. Any of ARRAY_A | ARRAY_N | OBJECT | OBJECT_K constants.
-	 *                                 ARRAY_N returns an array of rows indexed from 0.
-	 *                                 ARRAY_A returns an associative array (key => value, ...),
-	 *                                 with the key set to a snake_case version of the XML field name.
-	 *                                 OBJECT and OBJECT_K return an instance of the Pet class with
-	 *                                 variables keyed to the XML field name. Duplicate keys are discarded.
+	 * @param SimpleXMLElement $xml   The XML containing the pet fields.
+	 * @param string           $xpath The xpath to search for the pets collection.
 	 * @return array|\WP_Error An array of pet data, formatted according to $output.
 	 */
-	public function xml_to_pets( SimpleXMLElement $xml, string $xpath, $output = OBJECT ) {
+	public function xml_to_pets( SimpleXMLElement $xml, string $xpath ) {
 		$pets     = array();
 		$pets_xml = $xml->xpath( $xpath );
 
@@ -103,19 +94,8 @@ class PGParser {
 			return new WP_Error( $logger::ERROR, $error_message, $error_data );
 		}
 
-		$output_types = array( ARRAY_A, ARRAY_N, OBJECT, OBJECT_K );
-		if ( empty( $output ) || ! in_array( $output, $output_types, true ) ) {
-			$output = OBJECT;
-		}
-
 		foreach ( $pets_xml as $pet_xml ) {
-			$pet = $this->xml_to_pet( $pet_xml );
-
-			if ( ARRAY_N === $output || ARRAY_A === $output ) {
-				$pet = $this->pet_to_array( $pet, $output );
-			}
-
-			$pets[] = $pet;
+			$pets[] = $this->xml_to_pet( $pet_xml );
 		}
 
 		return $pets;
--- src/Pet.php
+++ src/Pet.php
@@ -9,37 +9,120 @@ namespace BigWing\PetAndGo;
  */
 class Pet {
 
+	use Utilities;
+
+	/**
+	 * Store for API data.
+	 *
+	 * @var array $data
+	 */
+	private $data = array();
+
 	/**
 	 * @return $this
 	 */
-	public function __construction() {
+	public function __construct() {
 		return $this;
 	}
 
 	/**
-	 * Get the currently set variables for the class.
-	 * @return string[]
+	 * @see Pet::get_field()
+	 *
+	 * @param string $name The API field name.
+	 * @return mixed|string The field value; empty string if field isn't set.
 	 */
-	public function get_fields(): array {
-		$fields = array();
-		foreach ( get_object_vars( $this ) as $key => $val ) {
-			$fields[] = $key;
-		}
-
-		return $fields;
-	}
-
-	public function get_field( string $field ) {
-		return $this->$field;
+	public function __get( string $name ) {
+		return $this->get_field( $name );
 	}
 
 	/**
-	 * @param string|true $data
+	 * @see Pet::set_field()
+	 *
+	 * @param string $name  The field name.
+	 * @param mixed  $value The field value.
 	 */
-	public function set_field( string $field, $data ) {
-		$this->$field = $data;
+	public function __set( string $name, $value ) {
+		$this->set_field( $name, $value );
 	}
 
+	/**
+	 * Removes a field from the data store.
+	 *
+	 * @param string $name The API field name.
+	 */
+	public function __unset( string $name ) {
+		$this->data = array_diff_key( $this->data, [ $name => $this->data[ $name ] ] );
+	}
+
+	/**
+	 * Get the currently set variables for the class.
+	 *
+	 * @return string[]
+	 */
+	public function get_field_names(): array {
+		return array_keys( $this->data );
+	}
+
+	/**
+	 * Gets the details for this pet.
+	 *
+	 * @param string $key_case Optional. Defines the type of array key used.
+	 *                         'snake'|'snake_case' converts the API XML field names to WP-style lowercase snake_case.
+	 *                         'camel'|'CamelCase' retains the original API XML field names.
+	 *                         Default 'camel'.
+	 * @return array A keyed array of data for this pet, keyed to $key_case format.
+	 */
+	public function get_pet_details_array( string $key_case = 'camel' ): array {
+		$details   = $this->data;
+		$key_case  = strtolower( $key_case );
+		$key_cases = array( 'snake', 'snake_case', 'camel', 'CamelCase' );
+
+		if ( empty( $key_case ) || ! in_array( $key_case, $key_cases, true ) ) {
+			$key_case = 'camel';
+		}
+
+		if ( 'snake' === $key_case || 'snake_case' === $key_case ) {
+			$snake = array();
+			foreach ( $details as $key => $value ) {
+				$snake[ $this->decamelize( $key ) ] = $value;
+				$this->__unset( $key );
+			}
+
+			$details = $snake;
+		}
+
+		return $details;
+	}
+
+	/**
+	 * Gets the data for a given API field name.
+	 *
+	 * @param string $field The field name.
+	 * @return mixed|string The field data; empty string if field not set.
+	 */
+	public function get_field( string $field ) {
+		if ( array_key_exists( $field, $this->data ) ) {
+			return $this->data[ $field ];
+		}
+
+		return '';
+	}
+
+	/**
+	 * Sets the data store for a given field.
+	 *
+	 * @param string $field The field name.
+	 * @param mixed  $data  The field data.
+	 */
+	public function set_field( string $field, $data ) {
+		$this->data[ $field ] = $data;
+	}
+
+	/**
+	 * Checks the Location field text to determine if a pet is available for adoption today.
+	 *
+	 * @return bool True if they are at the adoption center or an event; false if they are not.
+	 */
 	public function is_adoptable_today(): bool {
 		$location = $this->get_field( 'Location' );
 
@@ -62,8 +145,12 @@ class Pet {
 		return true;
 	}
 
+	/**
+	 * Checks the API to see if they are a featured pet today.
+	 *
+	 * @return bool True if the featured flag is set in the API; false if not.
+	 */
 	public function is_featured(): bool {
-		return 'No' !== $this->get_field( 'Featured' );
+		return (bool) stripos( $this->get_field( 'Featured' ), 'y' );
 	}
-
 }
